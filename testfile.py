e369c3cd20390a5621c74c6bee96a7b5851bf3cf
Wolftess <46814320+Wolftess@users.noreply.github.com>
2023-10-31 18:05:29 +0100

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -1,3 +1,17 @@
 import streamlit as st
-
-st.write('Hello Mara, wia geats dir?')
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import os
+import numpy as np
+import pandas as pd
+from tensorflow.keras.layers import Dense, Input, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.losses import sparse_categorical_crossentropy, categorical_crossentropy
+import transformers
+from transformers import TFAutoModel, AutoTokenizer
+from tqdm.notebook import tqdm
+from tokenizers import Tokenizer, models, pre_tokenizers, decoders, processors
+import pickle as pkl
